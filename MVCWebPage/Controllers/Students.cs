56545a142ee93ae0d29d742071b7100179f4bb5b
António Velez <antoniogvelez07@gmail.com>
2023-11-02 18:47:10 +0000

    Add Project

---
+++ MVCWebPage/Controllers/Students.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+
+namespace WebApplication2.Controllers
+{
+    public class Students : Controller
+    {
+        private static List<StudentsModel> students = new List<StudentsModel>();
+
+        // GET: Students
+        public ActionResult Index()
+        {
+            return View(students);
+        }
+
+        // GET: Students/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Students/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Students/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(StudentsModel student)
+        {
+            if (ModelState.IsValid)
+            {
+                student.Id = students.Count() == 0 ? 1 : students.Max(x => x.Id) + 1;
+                students.Add(student);
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+        // GET: Students/Edit/5
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var studentToEdit = students.FirstOrDefault(s => s.Id == id);
+
+            return View(studentToEdit);
+        }
+
+        // POST: Students/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(StudentsModel studentToEdit)
+        {
+            var studentToEditModel = students.FirstOrDefault(s => s.Id == studentToEdit.Id);
+            if (ModelState.IsValid)
+            {
+                studentToEditModel.Name = studentToEdit.Name;
+                studentToEditModel.LastName = studentToEdit.LastName;
+                studentToEditModel.Age = studentToEdit.Age;
+            }
+
+            return RedirectToAction("Index");
+
+        }
+
+
+        // GET: Students/Delete/5
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var studentToDelete = students.FirstOrDefault(s => s.Id == id);
+
+            return View(studentToDelete);
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(StudentsModel studentToDelete)
+        {
+            students.RemoveAll(s => s.Id == studentToDelete.Id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Search(string searchTerm)
+        {
+            if (searchTerm != null)
+            {
+                var searchStudent = students.Where(s => s.Name.Contains(searchTerm) || s.LastName.Contains(searchTerm)).ToList();
+                return View("Index", searchStudent);
+            }
+            else
+            {
+                // Handle the case when searchTerm is null or empty
+                return RedirectToAction("Index");
+            }
+        }
+
+    }
+}
---
+++ MVCWebPage/Models/StudentsModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication2.Models
+{
+    public class StudentsModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
